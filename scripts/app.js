fa724a6f5ad0de41a8c069f2159cba0f33ff6f3d
Mark Mwima <markmwimar@gmail.com>
2024-03-15 00:28:03 +0300

    updating data

--- scripts/app.js
+++ scripts/app.js
@@ -8,19 +8,31 @@ const TodosApp = {
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      const newTodo = {
-        text: this.enteredTodoText,
-        id: new Date().toISOString(),
-        editedTodoId: null,
-      };
+      if (this.editedTodoId) {
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return todoItem.id === this.editedTodoId;
+        });
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
 
-      this.todos.push(newTodo);
+        this.todos[todoIndex] = updatedTodoItem;
+      } else {
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: new Date().toISOString(),
+          editedTodoId: null,
+        };
+
+        this.todos.push(newTodo);
+      }
       enteredTodoText = "";
     },
     startEditTodo(todoId) {
       this.editedTodoId = todoId;
       const todo = this.todos.find(function (todoItem) {
-        return (todoItem.id = todoId);
+        return todoItem.id === todoId;
       });
       this.enteredTodoText = todo.text;
     },
